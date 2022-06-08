d53adb828a70a75ad7b8e5022f0a7ffe35697e29
Sudhir Dhangar <52579875+sudhirdhangar@users.noreply.github.com>
2022-06-08 16:56:08 +0530

    Create MiddleOfLinkedList.java

---
+++ MiddleOfLinkedList.java
@@ -0,0 +1,36 @@
+import java.util.* ;
+import java.io.*; 
+/****************************************************************
+
+    Following is the class structure of the Node class:
+
+       class Node 
+        { 
+        public:
+            int data;
+            Node next;
+            Node(int data) 
+            {
+              this.data = data;
+              this.next = null;
+            }
+        };
+
+*****************************************************************/
+
+public class Solution {
+    public static Node findMiddle(Node head) 
+    {
+        Node slow=head;
+        Node fast=head;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+          
+            fast=fast.next.next;
+        }
+        return slow;
+            
+        // Write your code here
+    }
+}
