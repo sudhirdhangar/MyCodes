a56985ff43e28743779cf72a2e614ac6a9c2ffd2
Sudhir Dhangar <52579875+sudhirdhangar@users.noreply.github.com>
2022-06-08 16:46:30 +0530

    Create ReverseLinkedList.java

---
+++ ReverseLinkedList.java
@@ -0,0 +1,33 @@
+import java.util.* ;
+import java.io.*; 
+/*
+	Following is the structure of the Singly Linked List.	
+	class LinkedListNode<T> 
+    {
+    	T data;
+    	LinkedListNode<T> next;
+    	public LinkedListNode(T data) 
+        {
+        	this.data = data;
+    	}
+	}
+
+*/
+public class Solution 
+{
+    public static LinkedListNode<Integer> reverseLinkedList(LinkedListNode<Integer> head) 
+    {
+        LinkedListNode prev=null,curr=head;
+        LinkedListNode next=null;
+        while(curr!=null)
+        {
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        head=prev;
+        // Write your code here!
+        return head;
+    }
+}
